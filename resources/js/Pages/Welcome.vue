1fc04fa667eb56a41cd1e34e20ffa37dee0ceebd
Wilber Galindez <wilbergalindezm23@gmail.com>
2024-05-05 20:10:19 +0200

    Migrations

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+
+defineProps({
+    canLogin: {
+        type: Boolean,
+    },
+    canRegister: {
+        type: Boolean,
+    },
+    laravelVersion: {
+        type: String,
+        required: true,
+    },
+    phpVersion: {
+        type: String,
+        required: true,
+    },
+});
+
+function handleImageError() {
+    document.getElementById('screenshot-container')?.classList.add('!hidden');
+    document.getElementById('docs-card')?.classList.add('!row-span-1');
+    document.getElementById('docs-card-content')?.classList.add('!flex-row');
+    document.getElementById('background')?.classList.add('!hidden');
+}
+</script>
+
+<template>
+    <Head title="Welcome" />
+    <div id="background" class="snap-x h-screen w-screen">
+        <div class="bg-gray-50 h-full snap-center text-black/50 dark:bg-black dark:text-white/50">
+            
+        </div>
+        <div class="bg-gray-50 h-full snap-center text-black/50 dark:bg-slate-800 dark:text-white/50">
+            
+        </div>    
+    </div>
+</template>
